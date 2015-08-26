3df1f6fb4c19ea076a1c0e1774cb05c43a39d049
spandanb <spandan.777@gmail.com>
2015-08-25 22:54:10 -0400

    Added fixed delay to outgoing packets

--- MSocket.java
+++ MSocket.java
@@ -3,27 +3,77 @@ import java.net.ServerSocket;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import static java.util.concurrent.TimeUnit.*;
+import java.util.Random;
+
+
 
 public class MSocket{
+    /*
+     * This class is used as a wrapper around sockets and streams.
+     * In addition to allowing network communication,
+     * it tracks statistics and can add delays and packet drops (possibly).
+     */
 
+    //For communication
     private Socket socket = null;
     private ServerSocket serverSocket = null;
     private ObjectInputStream in = null;
     private ObjectOutputStream out = null;
     
+    //For adding errors, like delays and packet reorders
+    private Random random = null;
+
+    //The queue of packets to send
+    private BlockingQueue sendingQueue = null;
+    
+    
+    private ScheduledExecutorService scheduler = null;
+    
+    /*
+     *The following internal classes are needed for async operations
+     */
+    class Sender implements Runnable{
+        private Object obj;
+        
+        public Sender(Object o){
+            this.obj = o;
+        }
+        
+        public void run(){
+            try{
+                //Access the outer classes' out
+                out.writeObject(obj);        
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            
+        }
+    }
+    
+
+    
+        
     /*
      *This creates a regular socket
      */
     public MSocket(String host, int port) throws IOException{
-        System.out.println("Entering MSocket constr");
         socket = new Socket(host, port);
-        System.out.println("Here 1");
-        in = new ObjectInputStream(socket.getInputStream());
-        System.out.println("Here 2");
+        //Be careful: outputStream should be initialized before
+        //inputStream, else it will block
         out = new ObjectOutputStream(socket.getOutputStream());
-        System.out.println("Leaving MSocket constr");
+        in = new ObjectInputStream(socket.getInputStream());
+        
+        sendingQueue = new LinkedBlockingQueue();
+        random = new Random(/*seed*/);
+        
+        scheduler = Executors.newScheduledThreadPool(10);
     }
-
+        
     /*
      *This creates a server socket
      */    
@@ -31,15 +81,28 @@ public class MSocket{
         
     }
     
-    public void writeObject(Object o) throws IOException{
-        System.out.println("MSocket:writeObject " + o);
+    public void writeObject(Object o) throws IOException{        
         out.writeObject(o);
     }
     
     public Object readObject() throws IOException, ClassNotFoundException{
-        System.out.println("MSocket:readObject ");
         return in.readObject();
     }
+    
+    public void writeObjectDelay(Object o){
+        //Delays the write by 1000 ms
+        scheduler.schedule(new Sender(o), 3000, MILLISECONDS);
+    }
+    
+    public void writeObjectUnordered(Object o){
+        //Changes the order of packets sent
+        scheduler.schedule(new Sender(o), 1000, MILLISECONDS);
+    }
+    
+    
+    
+    
+    
     
     public void close() throws IOException{
         in.close();
@@ -47,6 +110,17 @@ public class MSocket{
         socket.close();
     }
     
+    private void addDelay(){
+        //This block the whole code
+        try {
+            System.out.println("Sleeping");
+            Thread.sleep(1000);
+            System.out.println("Waking up");
+        } catch(InterruptedException ex) {
+            //System.out.println("Waking up");
+            //Thread.currentThread().interrupt();
+        }
+    }
     
 
 }
